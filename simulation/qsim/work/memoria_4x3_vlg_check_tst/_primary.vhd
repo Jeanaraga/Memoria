a05dcc775e1c29027554c8a1a50b2f4928a8f06a
Jean Pedro <jeanjesuspedrobook@gmail.com>
2025-07-04 01:50:10 -0300

    update memoria 4x3

---
+++ simulation/qsim/work/memoria_4x3_vlg_check_tst/_primary.vhd
@@ -0,0 +1,10 @@
+library verilog;
+use verilog.vl_types.all;
+entity memoria_4x3_vlg_check_tst is
+    port(
+        O0              : in     vl_logic;
+        O1              : in     vl_logic;
+        O2              : in     vl_logic;
+        sampler_rx      : in     vl_logic
+    );
+end memoria_4x3_vlg_check_tst;
